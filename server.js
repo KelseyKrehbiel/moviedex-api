40db3e0d8dd6cfbae7044fa8c9cf9aae4915e989
Klordok <kelsey@kelseykrehbiel.com>
2021-05-02 16:08:15 -0700

    add database and api token

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('hello')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIEDEX = require('./movies-data-small.json');
+
+console.log(process.env.API_TOKEN);
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
