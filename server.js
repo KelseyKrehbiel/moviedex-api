5cfb466844bdd036d4c9ac220fcf2615d2f97a4a
Klordok <kelsey@kelseykrehbiel.com>
2021-05-02 17:11:16 -0700

    add get handler ignore env

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-const helmet = require('helmet');
-const MOVIEDEX = require('./movies-data-small.json');
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIEDEX = require("./movies-data-small.json");
 
 console.log(process.env.API_TOKEN);
 
@@ -13,3 +13,33 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  console.log("validate bearer token middleware");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+function handleGetMovie(req, res) {
+  const film_title = req.query.film_title;
+
+  if (film_title) {
+    result = result.filter(
+      (item) => item.film_title.toLowerCase().indexOf(film_title) >= 0
+    );
+  }
+
+  res.json(result);
+}
+
+app.get("/movie", handleGetMovie);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
